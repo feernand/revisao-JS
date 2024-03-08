1d548fffdff48b183e3914770712f55b39692251
Fernando V Brito <fernandovieirabrito022@gmail.com>
2024-03-08 16:02:56 -0300

    subir revisao js

---
+++ type-booleano.js
@@ -0,0 +1,9 @@
+var cidade="Curitiba";
+
+cidade=cidade.toLowerCase;
+
+var cidadeNatal="curitiba";
+
+cidadeNatal=cidadeNatal.toLowerCase;
+
+console.log(cidade===cidadeNatal);
---
+++ type-string.js
@@ -0,0 +1,12 @@
+const nomeAluna = " Mia Romba";
+const maeAluna = " Deide costa";
+
+console.log(nomeAluna);
+
+const idadeAluna = 21;
+
+console.log(nomeAluna*idadeAluna);
+
+//podemos concatenar string e isso é
+//de certa forma uma operação entre strings
+console.log(nomeAluna+maeAluna);
